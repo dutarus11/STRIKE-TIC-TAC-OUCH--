ed8ba644234d6b8a96976a4cb4730f8de61dbc6d
dutarus11 <dutarus@gmail.com>
2022-10-23 16:40:39 -0700

    commit_8
    functionality 2

--- Tic-Toe-Ouch/Assets/Scripts/Cell.cs
+++ Tic-Toe-Ouch/Assets/Scripts/Cell.cs
@@ -7,6 +7,17 @@ public class Cell : MonoBehaviour
 {
     public event EventHandler click;
 
+    private TMPro.TextMeshPro text;
+
+
+    public void SetText(string _text) // set X O andSquare Objects 
+    {
+        //if (text == null)
+        //{
+        //    text = GetComponentInChildren<TMPro.TextMeshPro>();
+        //}
+        //text.text = _text;
+    }
     private void OnMouseDown()
     {
         click?.Invoke(this, EventArgs.Empty);
--- Tic-Toe-Ouch/Assets/Scripts/GameBoardController.cs
+++ Tic-Toe-Ouch/Assets/Scripts/GameBoardController.cs
@@ -2,10 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameBoardController : MonoBehaviour
 {
     public Cell[] cells;
+    public TMPro.TextMeshProUGUI token;
+    public Button newGame;
+    
+    private string[] tokens = new string[] { "X", "0" };
+    private int currentToken = 0;
+    
+    private TicTacToeOuchEngine engine = new TicTacToeOuchEngine();
     // Start is called before the first frame update
     void Awake()
     {
@@ -16,9 +24,17 @@ public class GameBoardController : MonoBehaviour
                   var index = Array.IndexOf(cells, o);
                   clicked(index);
               };
+            cell.SetText("");
         }
+        
     }
 
+    public void NewGame()
+    {
+        currentToken = 0;
+        token.text = tokens[currentToken];
+        newGame.interactable = false;
+    }
     private void clicked(int index)
     {
         print(index);
